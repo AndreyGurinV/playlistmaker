68458dfa52cf085016b069fa91bf830061e17bb9
Andrey Gurin <gurin.andreygv@yandex.ru>
2024-08-28 19:28:09 +0300

    Реализовано нажатие на кнопки

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val button = findViewById<Button>(R.id.find)
+        val imageClickListener: View.OnClickListener = object : View.OnClickListener { override fun onClick(v: View?) {
+            Toast.makeText(this@MainActivity, "Нажата кнопка Поиск", Toast.LENGTH_SHORT).show()
+        } }
+        button.setOnClickListener(imageClickListener)
+
+        findViewById<Button>(R.id.media).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка Медиатека", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<Button>(R.id.settings).setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажата кнопка Настройки", Toast.LENGTH_SHORT).show()
+        }
     }
+
 }
