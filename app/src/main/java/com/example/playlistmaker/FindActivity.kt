3cdfc7e7c12867244fc2de5f4a996e0d0c33071f
Andrey Gurin <gurin.andreygv@yandex.ru>
2024-09-16 11:10:36 +0300

    Третья часть

--- app/src/main/java/com/example/playlistmaker/FindActivity.kt
+++ app/src/main/java/com/example/playlistmaker/FindActivity.kt
@@ -1,12 +1,24 @@
 package com.example.playlistmaker
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class FindActivity : AppCompatActivity() {
+    private var searchText: String = FindActivity.SEARCH_TEXT_DEF
+    companion object {
+        const val SEARCH_TEXT = "SEARCH_TEXT"
+        const val SEARCH_TEXT_DEF = ""
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +27,55 @@ class FindActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        findViewById<Toolbar>(R.id.backFromFind).setOnClickListener {
+            finish()
+        }
+
+        val inputEditText = findViewById<EditText>(R.id.findEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+                searchText = s.toString()
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putString(SEARCH_TEXT, searchText)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle?,
+        persistentState: PersistableBundle?
+    ) {
+        super.onRestoreInstanceState(savedInstanceState, persistentState)
+        if (savedInstanceState != null) {
+            searchText = savedInstanceState.getString(SEARCH_TEXT, SEARCH_TEXT_DEF)
+            findViewById<EditText>(R.id.findEditText).setText(searchText)
         }
     }
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -27,7 +27,8 @@ class SettingsActivity : AppCompatActivity() {
 
         findViewById<TextView>(R.id.share).setOnClickListener {
             val shareIntent = Intent(Intent.ACTION_SEND)
-            shareIntent.putExtra(Intent.EXTRA_TEXT, R.string.link_for_practicum)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.link_for_practicum))
             startActivity(shareIntent)
         }
 
@@ -35,13 +36,13 @@ class SettingsActivity : AppCompatActivity() {
             val sendToIntent = Intent(Intent.ACTION_SENDTO)
             sendToIntent.data = Uri.parse("mailto:")
             sendToIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(R.string.my_email))
-            sendToIntent.putExtra(Intent.EXTRA_SUBJECT, R.string.subject_for_developers)
-            sendToIntent.putExtra(Intent.EXTRA_TEXT, R.string.message_for_developers)
+            sendToIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject_for_developers))
+            sendToIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.message_for_developers))
             startActivity(sendToIntent)
         }
 
         findViewById<TextView>(R.id.agreement).setOnClickListener {
-            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            val agreementIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_for_agreement)))
             startActivity(agreementIntent)
         }
     }
